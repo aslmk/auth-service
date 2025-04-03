2d532cc68af1c6eaa091bcbc96a96dd1e17d79ed
aslmk <aulaensov@gmail.com>
2025-04-03 17:03:35 +0500

    Initial commit

---
+++ src/test/java/com/aslmk/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aslmk.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
