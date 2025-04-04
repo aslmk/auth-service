c80918bde8195a6c5e0516ff17d98554c7201833
aslmk <aulaensov@gmail.com>
2025-04-04 12:19:03 +0500

    feat: add registration and login request dto.

---
+++ src/main/java/com/aslmk/authenticationservice/dto/RegistrationRequestDto.java
@@ -0,0 +1,23 @@
+package com.aslmk.authenticationservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegistrationRequestDto {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20)
+    private String username;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must contain at least 6 symbols")
+    private String password;
+    @NotBlank(message = "Please confirm your password")
+    private String confirmPassword;
+}
