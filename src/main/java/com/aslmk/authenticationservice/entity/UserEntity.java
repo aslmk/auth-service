bef01bffeaeda5807a052182f0efa58edd4af661
aslmk <aulaensov@gmail.com>
2025-04-05 21:36:14 +0500

    feat: create UserEntity and UserRole enum

---
+++ src/main/java/com/aslmk/authenticationservice/entity/UserEntity.java
@@ -0,0 +1,28 @@
+package com.aslmk.authenticationservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+}
