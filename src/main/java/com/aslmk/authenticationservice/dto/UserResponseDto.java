6f9fcc6f62e080b855797858d4bcd156bc3281f4
aslmk <aulaensov@gmail.com>
2025-04-06 21:27:38 +0500

    refactor: add Builder annotation to the UserResponseDto

--- src/main/java/com/aslmk/authenticationservice/dto/UserResponseDto.java
+++ src/main/java/com/aslmk/authenticationservice/dto/UserResponseDto.java
@@ -1,10 +1,12 @@
 package com.aslmk.authenticationservice.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class UserResponseDto {
     private long id;
     private String username;
