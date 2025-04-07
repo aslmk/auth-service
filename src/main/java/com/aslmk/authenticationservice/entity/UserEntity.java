28891f20745fee3983831f78180bbcb6d815568e
aslmk <aulaensov@gmail.com>
2025-04-07 20:07:42 +0500

    refactor: add Builder, NoArgsConstructor, AllArgsConstructor annotations to UserEntity

--- src/main/java/com/aslmk/authenticationservice/entity/UserEntity.java
+++ src/main/java/com/aslmk/authenticationservice/entity/UserEntity.java
@@ -1,12 +1,14 @@
 package com.aslmk.authenticationservice.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class UserEntity {
     @Id
