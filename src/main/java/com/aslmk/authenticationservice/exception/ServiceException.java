f2f89815e933f6588bdc446ff3943800d32d845b
aslmk <aulaensov@gmail.com>
2025-04-07 20:16:23 +0500

    feat: add custom exceptions ServiceException, UsernameAlreadyExistsException, EmailAlreadyExistsException

---
+++ src/main/java/com/aslmk/authenticationservice/exception/ServiceException.java
@@ -0,0 +1,7 @@
+package com.aslmk.authenticationservice.exception;
+
+public class ServiceException extends RuntimeException {
+    public ServiceException(String message) {
+        super(message);
+    }
+}
