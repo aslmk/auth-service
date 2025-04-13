cec3b98e2b74dca39de56078289ecf7127b7ca48
aslmk <aulaensov@gmail.com>
2025-04-13 15:46:41 +0500

    feat: implement AuthService and AuthServiceImpl

---
+++ src/main/java/com/aslmk/authenticationservice/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.aslmk.authenticationservice.service;
+
+import com.aslmk.authenticationservice.dto.LoginRequestDto;
+import com.aslmk.authenticationservice.dto.RegistrationRequestDto;
+import com.aslmk.authenticationservice.dto.UserResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface AuthService {
+    UserResponseDto registerUser(RegistrationRequestDto registrationRequestDto, HttpServletRequest request, HttpServletResponse response);
+    UserResponseDto authenticateUser(LoginRequestDto loginRequestDto, HttpServletRequest request, HttpServletResponse httpServletResponse);
+}
---
+++ src/main/java/com/aslmk/authenticationservice/service/Impl/AuthServiceImpl.java
@@ -0,0 +1,89 @@
+package com.aslmk.authenticationservice.service.Impl;
+
+import com.aslmk.authenticationservice.dto.LoginRequestDto;
+import com.aslmk.authenticationservice.dto.RegistrationRequestDto;
+import com.aslmk.authenticationservice.dto.UserResponseDto;
+import com.aslmk.authenticationservice.entity.UserEntity;
+import com.aslmk.authenticationservice.exception.UserNotFoundException;
+import com.aslmk.authenticationservice.service.AuthService;
+import com.aslmk.authenticationservice.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final SecurityContextRepository securityContextRepository;
+    private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager, UserService userService, SecurityContextRepository securityContextRepository) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+        this.securityContextRepository = securityContextRepository;
+    }
+
+    @Override
+    public UserResponseDto registerUser(RegistrationRequestDto registrationRequestDto,
+                                        HttpServletRequest httpRequest,
+                                        HttpServletResponse httpResponse) {
+
+        UserEntity userEntity = userService.saveUser(registrationRequestDto);
+
+        UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken
+                .unauthenticated(registrationRequestDto.getEmail(), registrationRequestDto.getPassword());
+
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        saveSecurityContext(authentication, httpRequest, httpResponse);
+
+        return UserResponseDto.builder()
+                .id(userEntity.getId())
+                .username(userEntity.getUsername())
+                .email(userEntity.getEmail())
+                .role(userEntity.getRole().toString())
+                .build();
+    }
+
+    @Override
+    public UserResponseDto authenticateUser(LoginRequestDto loginRequestDto,
+                                            HttpServletRequest httpRequest,
+                                            HttpServletResponse httpResponse) {
+
+        UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken
+                .unauthenticated(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        saveSecurityContext(authentication, httpRequest, httpResponse);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        UserEntity userEntity = userService.findUserByEmail(userDetails.getUsername()).orElseThrow(
+                () -> new UserNotFoundException("User " + userDetails.getUsername() + " not found")
+        );
+
+        return UserResponseDto.builder()
+                .id(userEntity.getId())
+                .username(userEntity.getUsername())
+                .email(userEntity.getEmail())
+                .role(userEntity.getRole().toString())
+                .build();
+    }
+
+    private void saveSecurityContext(Authentication authentication, HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
+        SecurityContext context = securityContextHolderStrategy.createEmptyContext();
+        context.setAuthentication(authentication);
+        securityContextHolderStrategy.setContext(context);
+        securityContextRepository.saveContext(context, httpRequest, httpResponse);
+    }
+}
