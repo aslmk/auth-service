0f814fa023b0ecf22c40063d82fd94d70059ae85
aslmk <aulaensov@gmail.com>
2025-11-01 11:50:47 +0500

    docs: add OpenApi documentation for UserController

--- src/main/java/com/aslmk/authenticationservice/controller/UserController.java
+++ src/main/java/com/aslmk/authenticationservice/controller/UserController.java
@@ -3,12 +3,21 @@ package com.aslmk.authenticationservice.controller;
 import com.aslmk.authenticationservice.dto.UserProfileDto;
 import com.aslmk.authenticationservice.entity.UserEntity;
 import com.aslmk.authenticationservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "User profile",
+        description = "Endpoints for retrieving and managing user profile information"
+)
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -19,6 +28,22 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Get authenticated user's profile",
+            description = """
+                    Returns detailed information about the currently authenticated user.
+                    """,
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User profile retrieved successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserProfileDto.class)
+                            )
+                    )
+            }
+    )
     @GetMapping("/profile")
     public ResponseEntity<UserProfileDto> profile(Authentication authentication) {
         String username = authentication.getName();
