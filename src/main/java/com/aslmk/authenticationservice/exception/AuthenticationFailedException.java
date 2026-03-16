c5d0f5305100a9d9fc701c8d3ba9ad74946b7fc8
aslmk <aulaensov@gmail.com>
2026-03-16 18:51:03 +0500

    refactor: remove obsolete "AuthenticationFailedException" handling

--- src/main/java/com/aslmk/authenticationservice/exception/AuthenticationFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.aslmk.authenticationservice.exception;
-
-public class AuthenticationFailedException extends RuntimeException {
-    public AuthenticationFailedException(String message) {
-        super(message);
-    }
-}
